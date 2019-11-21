2dd6346b49a23b9d91582be12a09383d903b9b92
Shelby Stanton <shelbylouise72@gmail.com>
2019-11-21 20:31:26 +0000

    feat: improving error handling of tcp connection

--- src/client/makeClient.js
+++ src/client/makeClient.js
@@ -7,15 +7,22 @@ export default function makeClient() {
   let client = new net.Socket();
 
   client.connect(8282, '127.0.0.1', function() {
-    console.log('Connected');
+    console.info('Connected');
   });
 
-  var stream = client.pipe(split());
+  const stream = client.pipe(split());
   stream.on('data', function(data) {
     processEvent(data);
   });
 
-  client.on('close', function() {
-    console.log('Connection closed');
+  client.on('close', function(hadError) {
+    if (hadError) {
+      console.warn('Encountered an error, retrying...');
+      setTimeout(makeClient, 1000);
+    }
+  });
+
+  client.on('end', function() {
+    console.info('End of stream connection closed');
   });
 }
